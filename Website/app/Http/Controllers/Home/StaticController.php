67ffadceb497b3d24acd1f4d6a9479a01dcea551
zc <292271704@qq.com>
2020-04-29 20:47:17 +0800

    学历3级分类

--- Website/app/Http/Controllers/Home/StaticController.php
+++ Website/app/Http/Controllers/Home/StaticController.php
@@ -120,8 +120,84 @@ class StaticController extends Controller
     //学历
     public function education()
     {
-        $education_data = DB::table('home_educations')->get();
+        //获取学历分类 以及 分类下对应的学校
+        //home_educations分类表
+        //home_education_adds分类对应地区表
+        //home_education_details分类对应学校表
 
+        //获取到"教育分类"和对应"分类地区"的leftJoin集合
+        $education_data = DB::table('home_educations')
+            ->leftJoin('home_education_adds','home_educations.id','=','home_education_adds.enducation_id')
+            ->get();
+
+        //获取到"地区"和对应"学校"的leftJoin集合
+        $education_adds_data = DB::table('home_education_adds')
+            ->leftJoin('home_education_details','home_education_adds.enducation_id','=','home_education_details.add_id')
+            ->get();
+
+        //获取类型数据
+        $educations = DB::table('home_educations')->get();
+
+//
+//        $meta =array();
+//        for($i = 0; $i < 3; $i++) {
+//            $tmp_url = $meta['quote'.($i+1)]=1;
+//        }
+//
+//  dd($tmp_url);
+
+
+//        dd($educations);
+
+        /**
+         * 编辑三级分类目录
+         */
+
+        //第一层 ： 类型层
+        foreach ($educations  as $e_key=>$e_val){
+
+
+              //第二层 ： 类型对应的区域层 ，使用 "教育分类"和对应" 集合数据
+            foreach ($education_data as $add_key=>$add_val){
+
+                //地区 enducation_id 与 分类id对应
+                if($e_val->id == $add_val->enducation_id){
+
+                    //数据集合
+                    $educations_mag[$e_key]['id'] = $add_val->id;
+                    $educations_mag[$e_key]['name'] = $add_val->name;
+                    $educations_mag[$e_key]['image'] =  $add_val->image;
+//                    $educations_mag[$e_key]['adds'][$add_key] = $add_val->address;
+
+//                    $educations_mag[$e_key]['adds'][$add_key] = 1;
+
+                    //获取对应地区的学校
+                    $add_schools = DB::table('home_education_details')
+                        ->where([
+                            ['add_id', '=', $e_val->id],
+                            ['enducation_add_id', '=', $add_val->enducation_id],
+                        ])
+//                        ->select('add_school_index')
+                        ->get();
+
+
+                            $adds_school_data = [];
+                    if($add_schools->count()){
+                        $adds_school_data['add'] = $add_val->address;
+                        $adds_school_data['school'] = $add_schools;
+                    }
+
+                    $educations_mag[$e_key]['adds'][$add_key] = $adds_school_data;
+
+                }
+                $aggregate = $educations_mag;
+            }
+        }
+
+
+dd($aggregate);
+exit;
+        dd($education_adds_data);
 
         $mag = $this->getNavigation();//获取导航栏数据
         return View('home.education')->with('column',$mag)->with('data',$education_data);
