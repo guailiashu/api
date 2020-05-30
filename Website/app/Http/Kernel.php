4c3ac9a040ba29ebed3bc8686a9e70f961725a0b
zc <292271704@qq.com>
2020-05-30 12:40:53 +0800

    跨域1.1

--- Website/app/Http/Kernel.php
+++ Website/app/Http/Kernel.php
@@ -42,7 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'cors-should',
+            \Medz\Cors\Laravel\Middleware\ShouldGroup::class,
         ],
     ];
 
@@ -64,7 +64,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors-should' => \Medz\Cors\Laravel\Middleware\ShouldGroup::class,
+        'cors' => \Medz\Cors\Laravel\Middleware\ShouldGroup::class,
     ];
 
     /**
